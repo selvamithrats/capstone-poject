7b444738bb36ee83e1d937e40d9e10d672a02fe8
SASI SASI <mithra15517@gmail.com>
2024-05-26 11:19:12 +0530

    first commit

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ document.getElementById('reliefForm').addEventListener('submit', function(event)
     // Gather form data
     const need = document.getElementById('need').value;
     const name = document.getElementById('name').value;
+    const contact = document.getElementById('contact').value;
     const address = document.getElementById('address').value;
     const description = document.getElementById('description').value;
 
     // For now, we'll just log the data to the console
-    console.log({ need, name, address, description });
+    console.log({ need, name, contact, address, description });
 
     // Display notification (in a real application, this is where you'd send the data to your server)
     document.getElementById('notificationArea').style.display = 'block';
